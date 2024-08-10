92a7402d16b99db481e060336dac559872baf189
kslai02 <158992231+kslai02@users.noreply.github.com>
2024-08-10 07:14:18 -0400

    Create firebase.js

---
+++ referral/firebase/firebase.js
@@ -0,0 +1,15 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyA3kLkvm0IxjE_8ivwoPNBJA9b8q2pvrs8",
+  authDomain: "on-tai.firebaseapp.com",
+  projectId: "on-tai",
+  storageBucket: "on-tai.appspot.com",
+  messagingSenderId: "609378442957",
+  appId: "1:609378442957:web:19aaa666beea5f2e1d5384",
+  measurementId: "G-GCPF9KPBEV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const db = firebase.firestore();
